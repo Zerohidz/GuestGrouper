7a21ac1f36ee38f1ba9e744460be0cf7c52741d9
Dursun Zahid Korkmaz <dursunzahidkorkmaz@gmail.com>
2023-11-02 18:31:43 +0300

    Kayıt Tamam

--- BlazorWebAssemblyTest/Server/Entities/Person.cs
+++ BlazorWebAssemblyTest/Server/Entities/Person.cs
@@ -5,6 +5,7 @@ namespace BlazorWebAssemblyTest.Server.Entities;
 public record Person(
     Person[]? GroupMembers = null,
     string? Name = null,
+    string? PhoneNumber = null,
     string? University = null,
     string? Department = null,
     string[]? Interests = null
@@ -15,6 +16,7 @@ public record Person(
         return new Person()
         {
             Name = model.Name,
+            PhoneNumber = model.PhoneNumber,
             Department = model.Department,
             Interests = model.Interests?.ToArray(),
             University = model.University
--- BlazorWebAssemblyTest/Shared/RegisterRequestModel.cs
+++ BlazorWebAssemblyTest/Shared/RegisterRequestModel.cs
@@ -3,6 +3,7 @@ namespace BlazorWebAssemblyTest.Shared;
 public class RegisterRequestModel
 {
     public string? Name { get; set; } = null;
+    public string? PhoneNumber { get; set; } = null;
     public string? University { get; set; } = null;
     public string? Department { get; set; } = null;
     public List<string>? Interests { get; set; } = null;
