6099ca06363c319a2725c8e4c10ef4065456dad5
Dursun Zahid Korkmaz <dursunzahidkorkmaz@gmail.com>
2023-11-02 19:06:51 +0300

    Kayıt Cinsiyetle Beraber Tamamen Tamam

--- BlazorWebAssemblyTest/Server/Entities/Person.cs
+++ BlazorWebAssemblyTest/Server/Entities/Person.cs
@@ -5,6 +5,7 @@ namespace BlazorWebAssemblyTest.Server.Entities;
 public record Person(
     Person[]? GroupMembers = null,
     string? Name = null,
+    Gender? Gender = null,
     string? PhoneNumber = null,
     string? University = null,
     string? Department = null,
@@ -16,6 +17,7 @@ public record Person(
         return new Person()
         {
             Name = model.Name,
+            Gender = model.Gender,
             PhoneNumber = model.PhoneNumber,
             Department = model.Department,
             Interests = model.Interests?.ToArray(),
--- BlazorWebAssemblyTest/Server/Managers/PersonManager.cs
+++ BlazorWebAssemblyTest/Server/Managers/PersonManager.cs
@@ -1,18 +1,23 @@
 using BlazorWebAssemblyTest.Server.Entities;
+using BlazorWebAssemblyTest.Shared;
 
 namespace BlazorWebAssemblyTest.Server.Managers;
 
 public class PersonManager
 {
-    private readonly List<Person> _clients = new();
+    private readonly List<Person> _maleClients = new();
+    private readonly List<Person> _femaleClients = new();
 
     public void AddPerson(Person person)
     {
-        _clients.Add(person);
+        if (person.Gender == Gender.Male)
+            _maleClients.Add(person);
+        else if (person.Gender == Gender.Female)
+            _femaleClients.Add(person);
     }
 
     public void MatchGroups()
     {
-        // TODO: 3lü gruplar halinde match'le
+        // TODO: 4lü gruplar halinde match'le
     }
 }
